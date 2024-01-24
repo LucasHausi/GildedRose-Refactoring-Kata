005e4396b5060c4c56bba99c12c05eb5284c44e6
LucasHausi <l.hausensteiner@gmail.com>
2024-01-24 12:20:03 +0100

    Summary: Add Parameterized Unit Tests for the basic decrement logic and adding comments for the found logic in the code
    Description: This commit includes tests for the behaviour:
    -Items SellIn decreases by one every day (Can get below 0)
    -Items Quality decreases by one every da, but not below 0

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,6 +11,7 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                //Decrease Quality for not special cases by one, but not below 0
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -35,7 +36,7 @@ class GildedRose {
                     }
                 }
             }
-
+            //Decrease SellIn for not special cases by one
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,51 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 0})
+    void givenNewItem_whenUpdatingQuality_thenInitialSellInValueDecrements(int initialSellIn) {
+        // Arrange
+        Item testItem = new Item("Sample Item", initialSellIn, 10);
+        Item[] testItems = {testItem};
+        GildedRose app = new GildedRose(testItems);
+        // Act
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        // Assert
+        assertEquals(initialSellIn-1, testItems[0].sellIn, "The SellIn value should be decreased by one after one day");
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 1})
+    void givenNewItem_whenUpdatingQuality_thenInitialQualityDecrements(int initialQuality) {
+        // Arrange
+        Item testItem = new Item("Sample Item", 5, initialQuality);
+        Item[] testItems = {testItem};
+        GildedRose app = new GildedRose(testItems);
+        // Act
+        app.updateQuality();
+        // Assert
+        assertEquals(initialQuality-1, testItems[0].quality, "The Quality value should be decreased by one after one day");
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1, 0, 3})
+    void givenNewItemWithZeroQuality_whenUpdatingQuality_thenInitialQualityDecrementsNotBelowZero(int initialQuality) {
+        // Arrange
+        Item testItem = new Item("Sample Item", 5, initialQuality);
+        Item[] testItems = {testItem};
+        GildedRose app = new GildedRose(testItems);
+        // Act
+        app.updateQuality();
+        app.updateQuality();
+        // Assert
+        assertTrue(testItems[0].quality >= 0, "The Quality value should be decreased by one after one day");
+    }
+
 
 }
